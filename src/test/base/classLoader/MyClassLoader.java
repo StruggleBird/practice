e4ff78f20ec40f073d3ea6cd9944f8781fa76057
StruggleBird <mjhx_zt@qq.com>
2014-12-08 16:06:53 +0800

    --

--- src/test/base/classLoader/MyClassLoader.java
+++ src/test/base/classLoader/MyClassLoader.java
@@ -10,6 +10,16 @@ public class MyClassLoader extends URLClassLoader {
 	}
 	
 	
-
+	public static void getCurrentClass(){
+		StackTraceElement[] sts = Thread.currentThread().getStackTrace();
+		for (StackTraceElement stackTraceElement : sts) {
+			System.out.println(stackTraceElement);
+		}
+    	 System.out.println(sts[2].getClassName());
+	}
+	
+	public static void main(String[] args) {
+		getCurrentClass();
+	}
 
 }
