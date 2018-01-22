6345db8e78607a6fe1d3d1b3a25d0a94d6a99b22
Ternence <mjhx_zt@qq.com>
2018-01-22 18:07:06 +0800

    添加ExecutorComplaetionService测试

--- src/test/thread/queue/LinkedBlockingQueueTest.java
+++ src/test/thread/queue/LinkedBlockingQueueTest.java
@@ -1,6 +1,7 @@
 package test.thread.queue;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -11,6 +12,8 @@ import org.junit.Test;
 public class LinkedBlockingQueueTest {
 	private ExecutorService executors = new ThreadPoolExecutor(100, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS,
 			new LinkedBlockingQueue<Runnable>());
+
+	private ExecutorCompletionService completionService = new ExecutorCompletionService(executors);
 
 	@Test
 	public void testSubmit() throws InterruptedException {
@@ -29,10 +32,77 @@ public class LinkedBlockingQueueTest {
 				}
 			});
 		}
+
 		System.out.println(Thread.currentThread().getThreadGroup().activeCount() + "========");
 		executors.shutdown();
 		executors.awaitTermination(10, TimeUnit.SECONDS);
 
+	}
+
+	/**
+	 * 测试目的，防止内存泄露
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testPoolCreated() throws InterruptedException {
+		while (true) {
+			executors = init();
+			for (int i = 0; i < 100; i++) {
+				final int taskId = i;
+				executors.submit(new Runnable() {
+
+					@Override
+					public void run() {
+						System.out.println(Thread.currentThread().getName() + " 执行 task:" + taskId);
+						try {
+							Thread.sleep(2);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+
+			System.out.println(Thread.currentThread().getThreadGroup().activeCount() + "========");
+			executors.shutdown();
+			executors.awaitTermination(10, TimeUnit.SECONDS);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testPoolReuse() throws InterruptedException {
+		while (true) {
+			for (int i = 0; i < 100; i++) {
+				final int taskId = i;
+				completionService.submit(new Runnable() {
+
+					@Override
+					public void run() {
+//						System.out.println(Thread.currentThread().getName() + " 执行 task:" + taskId);
+						try {
+							Thread.sleep(2);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}, null);
+			}
+			
+			for (int i = 0; i < 100; i++) {
+				completionService.take();
+			}
+			System.out.println("All task execution complated");
+
+		}
+	}
+
+	private ExecutorService init() {
+		executors = new ThreadPoolExecutor(100, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS,
+				new LinkedBlockingQueue<Runnable>());
+		return executors;
 	}
 
 }
