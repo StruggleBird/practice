e250ea630cfeeb03d745ac4128a72d8b8eca7cbe
Ternence <mjhx_zt@qq.com>
2018-02-24 15:07:53 +0800

    --

--- src/main/java/org/zt/test/jvm/PermGenOomMock.java
+++ src/main/java/org/zt/test/jvm/PermGenOomMock.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class PermGenOomMock {
 	public static void main(String[] args) throws Exception {
 
-		URL url = new File("/home/ternence/workspace/github/org.zt.test.commons/target/classes/org.zt.test/jvm").toURI().toURL();
+		URL url = new File("/home/ternence/workspace/github/sweet-java-lib/target/classes").toURI().toURL();
 		URL[] urls = { url };
 
 		// 获取有关类型加载的JMX接口
@@ -22,11 +22,10 @@ public class PermGenOomMock {
 			// 加载类型并缓存类加载器实例
 			ClassLoader classLoader = new URLClassLoader(urls);
 			classLoaders.add(classLoader);
-			classLoader.loadClass("org.zt.test.jvm.Test");
+			Class<?> class1 = classLoader.loadClass("org.zt.sweet.http.URIs");
+			
 			// 显示数量信息（共加载过的类型数目，当前还有效的类型数目，已经被卸载的类型数目）
-			System.out.println("total: " + loadingBean.getTotalLoadedClassCount());
-			System.out.println("active: " + loadingBean.getLoadedClassCount());
-			System.out.println("unloaded: " + loadingBean.getUnloadedClassCount());
+			System.out.printf("total:%s ,active: %s,unloaded: %s \r\n", loadingBean.getTotalLoadedClassCount(),loadingBean.getLoadedClassCount(),loadingBean.getUnloadedClassCount());
 		}
 	}
 }
--- src/main/java/org/zt/test/jvm/Test.java
+++ src/main/java/org/zt/test/jvm/Test.java
@@ -1,4 +1,17 @@
 package org.zt.test.jvm;
  
 public class Test {
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
 }
